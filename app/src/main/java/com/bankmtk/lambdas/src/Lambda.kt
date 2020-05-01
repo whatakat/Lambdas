fe25c1db4e04834f1a29e9d2de2a483875af1842
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-01 16:18:07 +0300

    test lambda

--- app/src/main/java/com/bankmtk/lambdas/src/Lambda.kt
+++ app/src/main/java/com/bankmtk/lambdas/src/Lambda.kt
@@ -1,6 +1,37 @@
 package com.bankmtk.lambdas.src
+typealias DoubleConversion = (Double) -> Double
+fun combine(lambda1: DoubleConversion,
+            lambda2: DoubleConversion): DoubleConversion{
+    return {x: Double ->lambda2(lambda1(x))}
+}
 
-fun main(args: Array<String>){
+fun convert(x: Double, converter:DoubleConversion): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+fun getConversionLambda(str: String):DoubleConversion {
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+    fun main(args: Array<String>) {
+
+        println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+        val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+        val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+        val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+        val value = 17.4
+        println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 //    var addFive = {x: Int ->x + 5}
 //    println("Pass 6 to addFive: ${addFive(6)}")
 //
@@ -25,28 +56,4 @@ fun main(args: Array<String>){
 //    }
 //    convert(20.0,{c:Double -> c*1.8+32})//a)
 //    convert(20.0){c:Double -> c*1.8+32} //b) it`s the same a)
-}
-
-typealias DoubleConversion = (Double) -> Double
-
-fun convert(x: Double, converter:DoubleConversion): Double {
-    val result = converter(x)
-    println("$x is converted to $result")
-    return result
-}
-fun getConversionLambda(str: String):DoubleConversion{
-    if (str == "CentigradeToFahrenheit"){
-        return {it*1.8 + 32}
-    }else if (str == "KgsToPounds"){
-        return {it*2.204623}
-    }else if (str == "PoundsToUSTons"){
-        return {it/2000.0}
-    }else{
-        return {it}
-    }
-    fun combine(lambda1: DoubleConversion,
-    lambda2: DoubleConversion): DoubleConversion{
-        return {x: Double ->lambda2(lambda1(x))}
-    }
-
 }
