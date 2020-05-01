7cad2571ae7179dcb415a8a721bd1b5cd5f63d84
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-01 12:18:17 +0300

    fun convert

--- app/src/main/java/com/bankmtk/lambdas/src/Lambda.kt
+++ app/src/main/java/com/bankmtk/lambdas/src/Lambda.kt
@@ -23,5 +23,6 @@ fun main(args: Array<String>){
         println("$x is converted to $result")
         return result
     }
-    convert(20.0,{c:Double -> c*1.8+32})
+    convert(20.0,{c:Double -> c*1.8+32})//a)
+    convert(20.0){c:Double -> c*1.8+32} //b) it`s the same a)
 }
