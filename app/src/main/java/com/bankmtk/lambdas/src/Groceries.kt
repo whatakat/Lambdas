f8a159c582bb185a00b35d81be9367fb97867526
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-02 18:36:44 +0300

    test maxBy, minBy, sumBy,sumByDouble

--- app/src/main/java/com/bankmtk/lambdas/src/Groceries.kt
+++ app/src/main/java/com/bankmtk/lambdas/src/Groceries.kt
@@ -13,4 +13,13 @@ fun main() {
         Grocery("Olive oil", "Pantry", "Bottle", 6.0, 1),
         Grocery("Ice cream", "Frozen", "Pack", 3.0, 2)
     )
+    val highestUnitPrice = groceries.maxBy { it.unitPrice*5 }
+    println("highestUnitPrice: $highestUnitPrice")
+    val lowestQuantity = groceries.minBy { it.quantity }
+    println("lowestQuantity: $lowestQuantity")
+
+    val sumQuantity = groceries.sumBy { it.quantity }
+    println("sumQuantity: $sumQuantity")
+    val totalPrice = groceries.sumByDouble { it.quantity*it.unitPrice }
+    println("totalPrice: $totalPrice")
 }
