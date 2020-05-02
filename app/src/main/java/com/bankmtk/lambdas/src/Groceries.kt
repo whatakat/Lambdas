290c3304b46162de4c9bba98d1175bf465dd757b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-02 18:29:29 +0300

    add lest

--- app/src/main/java/com/bankmtk/lambdas/src/Groceries.kt
+++ app/src/main/java/com/bankmtk/lambdas/src/Groceries.kt
@@ -1,4 +1,16 @@
 package com.bankmtk.lambdas.src
 
-data class Grocery(val name: String, val category: String,
-val unit: String, val unitPrice: Double, val quantity: Int)
+data class Grocery(
+    val name: String, val category: String,
+    val unit: String, val unitPrice: Double, val quantity: Int
+)
+
+fun main() {
+    val groceries = listOf(
+        Grocery("Tomatoes", "Vegetable", "lb", 3.0, 3),
+        Grocery("Mushrooms", "Vegetable", "lb", 4.0, 1),
+        Grocery("Bagels", "Bakery", "Pack", 1.5, 2),
+        Grocery("Olive oil", "Pantry", "Bottle", 6.0, 1),
+        Grocery("Ice cream", "Frozen", "Pack", 3.0, 2)
+    )
+}
